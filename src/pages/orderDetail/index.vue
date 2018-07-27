eb90efd8a1e55c399fd79658864eb1efe9118096
wangziwei@zdongpin.com <wangziwei@zdongpin.com>
2018-07-27 11:52:18 +0800

    静态资源

--- src/pages/orderDetail/index.vue
+++ src/pages/orderDetail/index.vue
@@ -12,11 +12,17 @@
               复制编号
             </div>
           </div>
-         
+          <div>
+            {{firstStatusName}}
+          </div>
         </div>
       </div>
+      <div class="buttonsArea">
+        <div class="buttonGroup">
+         
+        </div>
+      </div> 
     </div>
-    
   </div>
 </template>
 
@@ -26,7 +32,7 @@ export default {
   data(){
     return{
       orderNo:"", // 订单编号
-      firstStatusName: '', //订单状态
+      firstStatusName: '订单已完成', //订单状态
       order_type: null, // 订单状态
       appraise: null, // 评价状态
       // 订单退款退货状态
@@ -67,18 +73,41 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .orderLine{
-    font-size: 16px;
-    display: flex;
-    justify-content: space-around;
-    .copy{
-      width: 150rpx;
-      height: 50rpx;
-      line-height: 50rpx;
-      border: 1rpx solid #999;
-      border-radius: 50rpx;
-      text-align: center;
-      color: #999;
+  .body-content{
+    background: #eee;
+    position:absolute;
+    top:0;
+    left:0;
+    bottom:0;
+    right:0;
+    .status-item{
+       height: 50px;
+      padding: 20px 5px;
+      background: #fff;
+      font-size: 16px;
+      &>div{
+        margin-bottom: 10px;
+        &:nth-of-type(2){
+          color: #ff0000;
+          padding: 0 5px;
+        }
+      }
+    }
+    .orderLine{
+     
+      display: flex;
+      justify-content: space-around;
+      
+      .copy{
+        width: 150rpx;
+        height: 50rpx;
+        line-height: 50rpx;
+        border: 1rpx solid #999;
+        border-radius: 10rpx;
+        text-align: center;
+        color: #999;
+      }
     }
   }
+ 
 </style>
