846b862e7b07119ea112ccdf507ea0e49be4c6f8
wangziwei@zdongpin.com <wangziwei@zdongpin.com>
2018-07-30 15:25:35 +0800

    eslint

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -33,83 +33,77 @@
 </template>
 
 <script>
-import {order} from '@/http/http'
-import HeadTab from '@/components/headTab'
-import OrderItem from '@/components/orderItem'
-import OrderCarcle from '@/components/orderCarcle'
+import { order } from "@/http/http";
+import HeadTab from "@/components/headTab";
+import OrderItem from "@/components/orderItem";
+import OrderCarcle from "@/components/orderCarcle";
 export default {
-  data(){
-    return{
-      isMore:false, //是否可以加载更多(默认不允许以免滚动条滚动加载太多)
-      page:0,//默认加载页数
-      choiceTab:0,  //选中的页签
-      pageSize:10, //每次请求加载数量
+  data() {
+    return {
+      isMore: false, //是否可以加载更多(默认不允许以免滚动条滚动加载太多)
+      page: 0, //默认加载页数
+      choiceTab: 0, //选中的页签
+      pageSize: 10, //每次请求加载数量
       // 订单列表
-      orderList:[
-      
-      ],
+      orderList: [],
       // 页签信息
-      tabInfo:[
+      tabInfo: [
         {
-          title:"全部",
-          value:null,
-          name:"all"
+          title: "全部",
+          value: null,
+          name: "all"
         },
         {
-          title:"待付款",
-          value:1,
-          name:"pending_payment",
+          title: "待付款",
+          value: 1,
+          name: "pending_payment"
         },
         {
-          title:"待发货",
-          value:2,
-          name:"shipment_pending",
+          title: "待发货",
+          value: 2,
+          name: "shipment_pending"
         },
         {
-          title:"待收货",
-          value:3,
-          name:"waiting_for_delivery",
+          title: "待收货",
+          value: 3,
+          name: "waiting_for_delivery"
         },
         {
-          title:"待评价",
-          value:4,
-          name:"waiting_for_evaluation",
+          title: "待评价",
+          value: 4,
+          name: "waiting_for_evaluation"
         }
       ],
       // 是否显示取消订单
-      carcleShow:false,
+      carcleShow: false,
       // 取消订单订单号
-      carcleNumber:""
-    }
+      carcleNumber: ""
+    };
   },
-  computed: {
-    
-  },
+  computed: {},
   methods: {
     // 切换页签
-    changeTab(index){  
-      this.choiceTab=index;
-      this.page= 0
-      this.goods=[]
-      this.getData()
-      
+    changeTab(index) {
+      this.choiceTab = index;
+      this.page = 0;
+      this.goods = [];
+      this.getData();
     },
     // 加载更多
-    loadmore(){
-      if(this.isMore){
-        this.goods= this.goods.concat([
-          {name:"小吃11"},
-          {name:"小吃12"},
-          {name:"小吃13"},
-          {name:"小吃14"},
-          {name:"小吃15"},
-        ])
-        this.isMore=false;
+    loadmore() {
+      if (this.isMore) {
+        this.goods = this.goods.concat([
+          { name: "小吃11" },
+          { name: "小吃12" },
+          { name: "小吃13" },
+          { name: "小吃14" },
+          { name: "小吃15" }
+        ]);
+        this.isMore = false;
       }
-      
     },
     // 获取数据
-    getData(){
+    getData() {
       // console.log(order)
       // order.getItems({
       //   user_id:"1111",
@@ -122,184 +116,164 @@ export default {
         data: {
           order_infos: [
             {
-              'shop_id': 12,
-              order_time: '2017-11-11 12:21',
-              'order_num': '2-1452958295-Ucey8XJF',
-              'seller_shop_name': '测试号杨氏冷冻',
-              'seller_phone': '18280933708',
-              'main_status': 1,
-              'buy_status': 0,
-              'other_status': null,
-              'appraise': 3,
-              'goods_kind': 1,
-              'all_money': '6000.00',
-              'goods_info': [
+              shop_id: 12,
+              order_time: "2017-11-11 12:21",
+              order_num: "2-1452958295-Ucey8XJF",
+              seller_shop_name: "测试号杨氏冷冻",
+              seller_phone: "18280933708",
+              main_status: 1,
+              buy_status: 0,
+              other_status: null,
+              appraise: 3,
+              goods_kind: 1,
+              all_money: "6000.00",
+              goods_info: [
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '200.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "200.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 },
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 },
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 },
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 }
               ],
-              'refund': {
-                'buyer_apply_reason': '321',
-                'seller_refuse_reason': '陈浩州',
-                'money': '0.00',
-                'evidence': [
-                  'dada/saddsada.jpg',
-                  'deda.jpg',
-                  'dadsd.jpg'
-                ]
+              refund: {
+                buyer_apply_reason: "321",
+                seller_refuse_reason: "陈浩州",
+                money: "0.00",
+                evidence: ["dada/saddsada.jpg", "deda.jpg", "dadsd.jpg"]
               }
             },
             {
               shop_id: 2,
-              order_time: '2017-11-11 12:21',
-              'order_num': '2-1452958295-Ucey8XJF',
-              'seller_shop_name': '测试号杨氏冷冻',
-              'seller_phone': '18280933708',
-              'main_status': 2,
-              'buy_status': 10,
-              'other_status': 16,
-              'appraise': 3,
-              'goods_kind': 1,
-              'all_money': '6000.00',
-              'goods_info': [
+              order_time: "2017-11-11 12:21",
+              order_num: "2-1452958295-Ucey8XJF",
+              seller_shop_name: "测试号杨氏冷冻",
+              seller_phone: "18280933708",
+              main_status: 2,
+              buy_status: 10,
+              other_status: 16,
+              appraise: 3,
+              goods_kind: 1,
+              all_money: "6000.00",
+              goods_info: [
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 }
               ],
-              'refund': {
-                'buyer_apply_reason': '321',
-                'seller_refuse_reason': '陈浩州',
-                'money': '0.00',
-                'evidence': [
-                  'dada/saddsada.jpg',
-                  'deda.jpg',
-                  'dadsd.jpg'
-                ]
+              refund: {
+                buyer_apply_reason: "321",
+                seller_refuse_reason: "陈浩州",
+                money: "0.00",
+                evidence: ["dada/saddsada.jpg", "deda.jpg", "dadsd.jpg"]
               }
             },
             {
               shop_id: 9,
-              order_time: '2017-11-11 12:21',
-              'order_num': '2-1452958295-Ucey8XJF',
-              'seller_shop_name': '测试号杨氏冷冻',
-              'seller_phone': '18280933708',
-              'main_status': 4,
-              'buy_status': 10,
-              'other_status': 16,
-              'appraise': 2,
-              'goods_kind': 1,
-              'all_money': '6000.00',
-              'goods_info': [
+              order_time: "2017-11-11 12:21",
+              order_num: "2-1452958295-Ucey8XJF",
+              seller_shop_name: "测试号杨氏冷冻",
+              seller_phone: "18280933708",
+              main_status: 4,
+              buy_status: 10,
+              other_status: 16,
+              appraise: 2,
+              goods_kind: 1,
+              all_money: "6000.00",
+              goods_info: [
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 }
               ],
-              'refund': {
-                'buyer_apply_reason': '321',
-                'seller_refuse_reason': '陈浩州',
-                'money': '0.00',
-                'evidence': [
-                  'dada/saddsada.jpg',
-                  'deda.jpg',
-                  'dadsd.jpg'
-                ]
+              refund: {
+                buyer_apply_reason: "321",
+                seller_refuse_reason: "陈浩州",
+                money: "0.00",
+                evidence: ["dada/saddsada.jpg", "deda.jpg", "dadsd.jpg"]
               }
             },
             {
               shop_id: 8,
-              order_time: '2017-11-11 12:21',
-              'order_num': '2-1452958295-Ucey8XJF',
-              'seller_shop_name': '测试号杨氏冷冻',
-              'seller_phone': '18280933708',
-              'main_status': 4,
-              'buy_status': 1,
-              'other_status': 16,
-              'appraise': 4,
-              'goods_kind': 1,
-              'all_money': '6000.00',
-              'goods_info': [
+              order_time: "2017-11-11 12:21",
+              order_num: "2-1452958295-Ucey8XJF",
+              seller_shop_name: "测试号杨氏冷冻",
+              seller_phone: "18280933708",
+              main_status: 4,
+              buy_status: 1,
+              other_status: 16,
+              appraise: 4,
+              goods_kind: 1,
+              all_money: "6000.00",
+              goods_info: [
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 }
               ],
-              'refund': {
-                'buyer_apply_reason': '321',
-                'seller_refuse_reason': '陈浩州',
-                'money': '0.00',
-                'evidence': [
-                  'dada/saddsada.jpg',
-                  'deda.jpg',
-                  'dadsd.jpg'
-                ]
+              refund: {
+                buyer_apply_reason: "321",
+                seller_refuse_reason: "陈浩州",
+                money: "0.00",
+                evidence: ["dada/saddsada.jpg", "deda.jpg", "dadsd.jpg"]
               }
             },
             {
               shop_id: 89,
-              order_time: '2017-11-11 12:21',
-              'order_num': '2-1452958295-Ucey8XJF',
-              'seller_shop_name': '测试号杨氏冷冻',
-              'seller_phone': '18280933708',
-              'main_status': 3,
-              'buy_status': 2,
-              'other_status': 16,
-              'appraise': 3,
-              'goods_kind': 1,
-              'all_money': '6000.00',
-              'goods_info': [
+              order_time: "2017-11-11 12:21",
+              order_num: "2-1452958295-Ucey8XJF",
+              seller_shop_name: "测试号杨氏冷冻",
+              seller_phone: "18280933708",
+              main_status: 3,
+              buy_status: 2,
+              other_status: 16,
+              appraise: 3,
+              goods_kind: 1,
+              all_money: "6000.00",
+              goods_info: [
                 {
-                  'goods_name': '测试冻品',
-                  'unit_price': '500.00',
-                  'num': 5,
-                  'goods_img': '',
-                  'refund_num': 1
+                  goods_name: "测试冻品",
+                  unit_price: "500.00",
+                  num: 5,
+                  goods_img: "",
+                  refund_num: 1
                 }
               ],
-              'refund': {
-                'buyer_apply_reason': '321',
-                'seller_refuse_reason': '陈浩州',
-                'money': '0.00',
-                'evidence': [
-                  'dada/saddsada.jpg',
-                  'deda.jpg',
-                  'dadsd.jpg'
-                ]
+              refund: {
+                buyer_apply_reason: "321",
+                seller_refuse_reason: "陈浩州",
+                money: "0.00",
+                evidence: ["dada/saddsada.jpg", "deda.jpg", "dadsd.jpg"]
               }
             }
           ],
@@ -312,27 +286,24 @@ export default {
             waitAppraiseCount: 1
           }
         },
-        message: '获取成功'
-      }
-      this.orderList=this.orderList.concat(res.data.order_infos)
+        message: "获取成功"
+      };
+      this.orderList = this.orderList.concat(res.data.order_infos);
       // }).catch(err=>{
-          // console.log("请求订单列表错误",err)
+      // console.log("请求订单列表错误",err)
       // })
-
     },
     // 打开取消订单
-    openCarcle(orderNumber){
-      wx.hideTabBar()
-      this.carcleShow=true;
+    openCarcle(orderNumber) {
+      wx.hideTabBar();
+      this.carcleShow = true;
     },
     // 提交取消订单
-    commitCarcle(){
-
-    },
+    commitCarcle() {},
     // 关闭取消订单
-    closeCarcle(){
-      this.carcleShow=false;
-      wx.showTabBar()
+    closeCarcle() {
+      this.carcleShow = false;
+      wx.showTabBar();
     }
   },
   components: {
@@ -340,31 +311,31 @@ export default {
     HeadTab,
     OrderCarcle
   },
-  onLoad(){
-    wx.showTabBar()
+  onLoad() {
+    wx.showTabBar();
     // wx.hideTabBar()
-    this.getData()
+    this.getData();
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
- .mainBox{
-   background: #f5f5f5;
-  position:absolute;
-  top:0;
-  left:0;
-  bottom:0;
-  right:0;
-  .title{
+.mainBox {
+  background: #f5f5f5;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  .title {
     height: 5%;
   }
-  .orderBox{
-    background:#f5f5f5;
-    .orderItem{
+  .orderBox {
+    background: #f5f5f5;
+    .orderItem {
       background: #fff;
       // padding: 5rpx 0;
     }
   }
- }
+}
 </style>
--- src/pages/orderDetail/index.vue
+++ src/pages/orderDetail/index.vue
@@ -39,46 +39,45 @@
 </template>
 
 <script>
-import OrderCarcle from '@/components/orderCarcle'
-import {mapGetters} from 'vuex'
+import OrderCarcle from "@/components/orderCarcle";
+import { mapGetters } from "vuex";
 export default {
-  data(){
-    return{
-      orderNum:"", // 订单编号
-      firstStatusName: '订单已完成', //订单状态
+  data() {
+    return {
+      orderNum: "", // 订单编号
+      firstStatusName: "订单已完成", //订单状态
       order_type: null, // 订单状态
       appraise: null, // 评价状态
       // 订单退款退货状态
       refund_order_type: null,
       orderData: {},
-      sellerPhone:"123456789",//卖家电话
-      carcleShow:false, //是否显示
-    }
+      sellerPhone: "123456789", //卖家电话
+      carcleShow: false //是否显示
+    };
   },
-   computed:{
-    ...mapGetters(['buttontype']),
-    buttonList(){
+  computed: {
+    ...mapGetters(["buttontype"]),
+    buttonList() {
       // return [0,1,2]
       return [
         {
-          type:0,
-          phone:this.sellerPhone
+          type: 0,
+          phone: this.sellerPhone
         },
         {
-          type:1,
-          orderNumber:this.orderNum
+          type: 1,
+          orderNumber: this.orderNum
         },
         {
-          type:2,
-          orderNumber:this.orderNum
+          type: 2,
+          orderNumber: this.orderNum
         }
-      ]
-    },
-    
+      ];
+    }
   },
   methods: {
-  // 复制到剪切板   
-    copyOrderNo(){
+    // 复制到剪切板
+    copyOrderNo() {
       let that = this;
       wx.setClipboardData({
         data: that.orderNum,
@@ -86,79 +85,75 @@ export default {
           wx.getClipboardData({
             success: function(res) {
               wx.showToast({
-                title: '复制成功',
-                icon: 'success',
+                title: "复制成功",
+                icon: "success",
                 duration: 500
-              })
+              });
             }
-          })
+          });
         }
-      })
+      });
     },
-    handleClick(item){
-      this.buttontype[item.type].fn(item,this)
+    handleClick(item) {
+      this.buttontype[item.type].fn(item, this);
     },
-    openCarcle(){
-      wx.hideTabBar()
-      this.carcleShow=true;
+    openCarcle() {
+      wx.hideTabBar();
+      this.carcleShow = true;
     },
-    commitCarcle(){
-
-    },
-    closeCarcle(){
-      this.carcleShow=false;
-      wx.showTabBar()
+    commitCarcle() {},
+    closeCarcle() {
+      this.carcleShow = false;
+      wx.showTabBar();
     }
   },
   components: {
     OrderCarcle
   },
-  onLoad(option){
-    wx.hideTabBar()
-    this.orderNum = option.orderNum
-    
+  onLoad(option) {
+    wx.hideTabBar();
+    this.orderNum = option.orderNum;
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-  .body-content{
-    background: #eee;
-    position:absolute;
-    top:0;
-    left:0;
-    bottom:0;
-    right:0;
-    .status-item{
-       height: 50px;
-      padding: 20px 5px;
-      background: #fff;
-      font-size: 16px;
-      &>div{
-        margin-bottom: 10px;
-        &:nth-of-type(2){
-          color: #ff0000;
-          padding: 0 5px;
-        }
+.body-content {
+  background: #eee;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  .status-item {
+    height: 50px;
+    padding: 20px 5px;
+    background: #fff;
+    font-size: 16px;
+    & > div {
+      margin-bottom: 10px;
+      &:nth-of-type(2) {
+        color: #ff0000;
+        padding: 0 5px;
       }
-    }
-    .orderLine{  
-      display: flex;
-      justify-content: space-around;
-      .copy{
-        width: 150rpx;
-        height: 50rpx;
-        line-height: 50rpx;
-        border: 1rpx solid #999;
-        border-radius: 10rpx;
-        text-align: center;
-        color: #999;
-      }
-    }
-    .buttonsArea{
-      position:absolute;
-      bottom:0;
     }
   }
- 
+  .orderLine {
+    display: flex;
+    justify-content: space-around;
+    .copy {
+      width: 150rpx;
+      height: 50rpx;
+      line-height: 50rpx;
+      border: 1rpx solid #999;
+      border-radius: 10rpx;
+      text-align: center;
+      color: #999;
+    }
+  }
+  .buttonsArea {
+    position: absolute;
+    bottom: 0;
+  }
+}
 </style>
